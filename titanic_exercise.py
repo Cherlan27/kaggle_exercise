81aa52c185537418b0c52d14354e5298c2fc54ae
Cherlan <l.petersdorf@gmail.com>
2024-03-22 21:22:02 +0100

    Added some Scaling Factor

--- titanic_exercise.py
+++ titanic_exercise.py
@@ -8,6 +8,7 @@ Created on Thu Mar 21 20:57:25 2024
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
 
 # Import data from source
 
@@ -20,11 +21,14 @@ print(train_data["Survived"].head(5))
 
 # Data Cleaning
 age_train = train_data["Sex"].replace("male",0).replace("female",1)
+fare_train = preprocessing.StandardScaler().fit_transform(np.array(train_data['Fare']).reshape(-1,1)).reshape(1,-1)[0]
+
+
 y_train = train_data["Survived"]
 #x_train = pd.concat([train_data["PassengerId"], train_data["Pclass"], train_data["Name"], train_data['Sex'], train_data['Age'], train_data['SibSp'],
 #       train_data['Parch'], train_data['Ticket'], train_data['Fare'], train_data['Cabin'], train_data['Embarked']], axis = 1)
 x_train = pd.concat([train_data["PassengerId"], train_data["Pclass"], age_train, train_data['Age'], train_data['SibSp'],
-       train_data['Parch'], train_data['Fare']], axis = 1)
+       train_data['Parch'], pd.DataFrame({"Fare": fare_train})], axis = 1)
 
 print(x_train.head(10))
 
@@ -37,10 +41,12 @@ print(clf.predict([[1, 3, 0, 22.0, 1, 0, 7.25]]))
 print(clf.predict([[2, 1, 1, 38.0, 1, 0, 71.2833]]))
 
 age_test = test_data["Sex"].replace("male",0).replace("female",1)
+fare_test = preprocessing.StandardScaler().fit_transform(np.array(test_data['Fare']).reshape(-1,1)).reshape(1,-1)[0]
+
 x_test = pd.concat([test_data["PassengerId"], test_data["Pclass"], age_test, test_data['Age'], test_data['SibSp'],
-       test_data['Parch'], test_data['Fare']], axis = 1)
+       test_data['Parch'], pd.DataFrame({"Fare": fare_test})], axis = 1)
 
 
 y_test = clf.predict(x_test)
-prediction = pd.concat([test_data["PassengerId"], pd.DataFrame(y_test, header = "Survived")], axis = 1)
-prediction.to_csv("prediction.csv", index = False)
+prediction = pd.concat([test_data["PassengerId"], pd.DataFrame({"Survived": y_test})], axis = 1)
+prediction.to_csv("prediction3.csv", index = False)
